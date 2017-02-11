b4cec7cd6d43cafab6398733eb1a656f55892b6f
Eric Zhang <ezhang2008@gmail.com>
2017-02-10 20:13:37 -0500

    newdb.js

---
+++ server/src/newDb.js
@@ -0,0 +1,175 @@
+
+export const items = [
+  {
+    id: 0,
+    item: 'Beer',
+    link: 'http://a.co/7g7y09h',
+    price: 10.01,
+    stars: 1
+  },
+  {
+    id: 1,
+    item: 'Mac Computers',
+    link: 'http://a.co/8Eqsqz0',
+    price: 2000.99,
+    stars: 1241
+  },
+  {
+    id: 2,
+    item: 'Tea',
+    link: 'http://a.co/gswlitg',
+    price: 5.43,
+    stars: 4121
+  },
+  {
+    id: 3,
+    item: 'Mechanical Keyboards',
+    link: 'http://a.co/9gafmM4',
+    price: 10.01,
+    stars: 321
+  },
+  {
+    id: 4,
+    item: 'Hoodies',
+    link: 'http://a.co/7Tw0DgC',
+    price: 20.00,
+    stars: 4523
+  },
+  {
+    id: 5,
+    item: 'Millenium Falcon',
+    link: 'http://a.co/8U68Krj',
+    price: 9999999,
+    stars: 2532
+  },
+  {
+    id: 6,
+    item: 'Snow',
+    link: 'http://a.co/8UUkPyk',
+    price: .01,
+    stars: 54
+  },
+  {
+    id: 7,
+    item: 'Whiskey',
+    link: 'http://a.co/0LnPx7L',
+    price: 123.45,
+    stars: 4521
+  },
+  {
+    id: 8,
+    item: 'Ferrari',
+    link: 'http://a.co/4hvdlLd',
+    price: 40.00,
+    stars: 8592375
+  },
+];
+
+export const reviews = [
+  {
+    id: 0,
+    user: 'joe schmo'
+    rating: 3,
+    comment: 'it was ok',
+    item: '3'
+  },
+  {
+    id: 1,
+    user: 'joe schmo'
+    rating: 5,
+    comment: 'it was great',
+    item: '5'
+  },
+  {
+    id: 2,
+    user: 'joe schmo'
+    rating: 3,
+    comment: 'it was acceptable',
+    item: '1'
+  },
+  {
+    id: 3,
+    user: 'joe schmos son'
+    rating: 1,
+    comment: 'I think you should really buy this as well!!!!!!!',
+    item: '2'
+  },
+  {
+    id: 4,
+    user: 'joe schmos father in law'
+    rating: 5,
+    comment: 'could have been worse',
+    item: '8'
+  },
+  {
+    id: 5,
+    user: 'calvin'
+    rating: 2,
+    comment: 'something something somethign garbage',
+    item: '6'
+  },
+  {
+    id: 6,
+    user: 'test user'
+    rating: 5,
+    comment: 'insert good review here',
+    item: '7'
+  },
+  {
+    id: 7,
+    user: 'joe schmos assistant'
+    rating: 5,
+    comment: 'wow this changed by life',
+    item: '0'
+  },
+  {
+    id: 8,
+    user: 'donald trump'
+    rating: 5,
+    comment: 'this will make america great again',
+    item: '2'
+  },
+  {
+    id: 9,
+    user: 'some guy'
+    rating: 2,
+    comment: 'this is some product',
+    item: '5'
+  },
+  {
+    id: 10,
+    user: 'donald trump'
+    rating: 4,
+    comment: 'Im not going to spread some false news about this, but it was pretty good',
+    item: '2'
+  },
+  {
+    id: 11,
+    user: 'calvin'
+    rating: 4,
+    comment: 'hahahahahahahahahahahahahaha',
+    item: '4'
+  },
+  {
+    id: 12,
+    user: 'joe schmo'
+    rating: 1,
+    comment: 'horrible product never again',
+    item: '1'
+  },
+  {
+    id: 13,
+    user: 'ms piggy'
+    rating: 4,
+    comment: 'kermit would love this',
+    item: '4'
+  },
+  {
+    id: 14,
+    user: 'kermit'
+    rating: 1,
+    comment: 'fewinuiewnfoweiufweifuw eiuofbwidnfuywgu eanifweionworugeriubgiuw',
+    item: '6'
+  },
+];
+
